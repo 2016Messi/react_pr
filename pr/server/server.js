81e64d147db421b3a12e4084f9a94683a1c45778
2016Messu <wy99599@163.com>
2018-01-10 11:38:11 +0800

    提交

--- pr/server/server.js
+++ pr/server/server.js
@@ -1,13 +1,44 @@
 const express = require('express');
+const mongoose = require('mongoose');
+//  连接mongo  并且使用react这个集合
+const DB_URL = 'mongodb://localhost:27017/react';
+mongoose.connect(DB_URL);
+mongoose.connection.on('connected',function() {
+    console.log(" mongo connect success ");
+});
 
-const app =express();
+const User = mongoose.model('user',new mongoose.Schema({
+    user:{type:String,require:true},
+    age:{type:Number,require:true}
+}));
+//  新增数据
+User.create({
+    user:'xiaoli',
+    age:18
+},function(err,doc) {
+    if(!err){
+        console.log(doc);
+    }else{
+        console.log(err);
+    }
+});
+
+//新建app
+
+User.remove({age:18},function(err,doc) {
+    console.log(doc);
+})
+
+const app = express();
 
 app.get('/',(req,res)=>{
     res.send('<h1>hello world1</h1>');
 })
 
 app.get('/data',(req,res)=>{
-    res.json({name:'wang',type:'web'});
+   User.find({},function(err,doc) {
+        return res.json(doc);
+   })
 })
 
 app.listen(9093,()=>(
